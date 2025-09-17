27599dd2956ae74c228920f9e31394451c045cc0
JustOMORI7 <162158604+JustOMORI7@users.noreply.github.com>
2025-09-17 21:03:52 +0300

    Update packages.nix

--- configs/packages.nix
+++ configs/packages.nix
@@ -6,6 +6,7 @@
     emacs wget git htop swaybg xfce.thunar xfce.thunar-volman gvfs
     waybar fuzzel alacritty pavucontrol playerctl brave audacious
     brightnessctl swaylock lxappearance nwg-look materia-theme
-    papirus-icon-theme bibata-cursors xwayland-satellite
+    papirus-icon-theme bibata-cursors xwayland-satellite tageditor
+    avizo
   ];
 }
