0c3feaeda997f1bebfdbc50e921c80a10d4777e8
JustOMORI7 <162158604+JustOMORI7@users.noreply.github.com>
2025-11-10 13:20:30 +0300

    Update packages.nix

--- configs/packages.nix
+++ configs/packages.nix
@@ -4,9 +4,12 @@
 {
   environment.systemPackages = with pkgs; [
     emacs wget git htop swaybg xfce.thunar xfce.thunar-volman gvfs
-    waybar fuzzel alacritty pavucontrol playerctl brave audacious
+    waybar fuzzel kitty pavucontrol playerctl firefox fastfetch
     brightnessctl swaylock lxappearance nwg-look materia-theme
     papirus-icon-theme bibata-cursors xwayland-satellite tageditor
-    avizo
+    avizo swayimg peazip libsForQt5.qtstyleplugin-kvantum ffmpeg
+    libsForQt5.qt5ct materia-kde-theme qbittorrent strawberry lutris-free
+    protonup-qt mako mpv unrar python313 kdePackages.kolourpaint
+    localsend
   ];
 }
