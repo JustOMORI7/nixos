d7a0bcb721104f6cff6b1ddd6132a4bcd345ff5c
Just.OMORI7 <iam.ghost755@proton.me>
2026-08-25 17:09:58 +0300

    switch from thunar to nemo

--- packages.nix
+++ packages.nix
@@ -4,12 +4,12 @@
   environment.systemPackages = with pkgs; [
     helix kitty rofi git wget python3 keepassxc playerctl ffmpeg
     waybar papirus-icon-theme orchis-theme fastfetch nwg-look eza
-    pavucontrol thunar thunar-volman gvfs hypridle brightnessctl
+    pavucontrol nemo gvfs hypridle brightnessctl grim slurp swappy
     xwayland-satellite wlogout materia-kde-theme mpv losslesscut
     libsForQt5.qt5ct swayimg hyfetch bibata-cursors wayfreeze btop
     appimage-run localsend heroic protonup-qt qbittorrent libnotify
     swaynotificationcenter wf-recorder ferdium cliphist wlopm fd fzf
     wl-clipboard hyprpicker imagemagick unzip unrar swaybg upscayl
-    grim slurp swappy flowblade lolcat cowsay mpc ymuse
+    flowblade lolcat cowsay mpc ymuse
   ];
 }
